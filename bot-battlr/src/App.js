e75e14b938737b703aba2ed7bb349304a2be8358
mwirigi <mwirigimburugu81@gmail.com>
2023-11-21 09:46:43 +0300

    completed

---
+++ bot-battlr/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import Display from './components/BotDisplay';
+//import BotsCard from './components/BotCard';
+//import BotCollection from './components/BotCollection';
+//import BotArmy from './components/YourBotArmy';
+
+function App() {
+  return (
+    <div className="App">
+    <Display/>
+    </div>
+  );
+}
+
+export default App;
